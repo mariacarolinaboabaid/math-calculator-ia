c03a7b99cc14314a06ce245f2f70af6ee0d2eb4f
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2024-10-31 16:06:08 -0300

    Fix environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,3 +3,4 @@ export const environment = {
     geminiApiKey: process.env['GEMINI_API_KEY'] || '',
     apiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent'
 }
+
