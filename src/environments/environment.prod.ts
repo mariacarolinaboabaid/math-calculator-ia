01ff5d08bbeb6f5755531cab98b31b8601876198
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2024-10-31 14:33:31 -0300

    Fix git ignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    geminiApiKey: process.env['GEMINI_API_KEY'] || '',
+    apiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent'
+}
